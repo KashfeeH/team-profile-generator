9b8881b4723230c9dec9bafa06ea23fded5818ec
KashfeeH <151862358+KashfeeH@users.noreply.github.com>
2024-02-25 11:48:15 +0000

    updating the main code into index.js

--- starter/index.js
+++ starter/index.js
@@ -13,3 +13,26 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+let EmployeeArray = [];
+
+const questions = [
+  {
+    type: 'input',
+    name: 'managerName',
+    message: "What is the manager's name?",
+    validate: (input) => input ? true : 'Manager name cannot be empty.',
+  },
+  {
+    type: 'input',
+    name: 'managerId',
+    message: "What is the manager's ID?",
+    validate: (input) => input ? true : 'ID cannot be empty.',
+  },
+  {
+    type: 'input',
+    name: 'managerEmail',
+    message: "What is the manager's email?",
+    validate: (email) => /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email) ? true : 'Please enter a valid email address.',
+  },
+];
+
--- starter/src/page-template.js
+++ starter/src/page-template.js
@@ -4,8 +4,8 @@ const generateTeam = team => {
     // creates the manager html
     const generateManager = manager => {
         return `
-        <div class="card employee-card">
-        <div class="card-header" style ="background-color: #2F4550;">
+        <div class="card employee-card" style="box-shadow: 5px 5px 5px gray; margin: 8px;">
+        <div class="card-header" style ="background-color: #2F4550; color: white;">
             <h2 class="card-title">${manager.getName()}</h2>
             <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
         </div>
@@ -23,8 +23,8 @@ const generateTeam = team => {
     // creates the html for engineers
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card">
-    <div class="card-header" style ="background-color: #003559;">
+        <div class="card employee-card" style="box-shadow: 5px 5px 5px gray; margin: 8px;">
+    <div class="card-header" style ="background-color: #003559; color: white;">
         <h2 class="card-title">${engineer.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
     </div>
@@ -42,8 +42,8 @@ const generateTeam = team => {
     // creates the html for interns
     const generateIntern = intern => {
         return `
-        <div class="card employee-card">
-    <div class="card-header" style ="background-color: #003559;">
+        <div class="card employee-card" style="box-shadow: 5px 5px 5px gray; margin: 8px;">
+    <div class="card-header" style ="background-color: #003559; color: white;">
         <h2 class="card-title">${intern.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
     </div>
@@ -100,7 +100,7 @@ module.exports = team => {
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading" style= "background-color: #0F110C; color: white;">
+            <div class="col-12 jumbotron mb-3 team-heading" style= "background-color: #2D3047; color: white;">
                 <h1 class="text-center">My Team</h1>
             </div>
         </div>
